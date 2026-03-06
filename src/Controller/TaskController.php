f9b1922b2212650c936948e93c5c396e1559678f
iased <ias.edina@gmail.com>
2026-03-06 15:20:21 +0200

    add task filtering by status

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -17,15 +17,36 @@ use Doctrine\ORM\EntityManagerInterface;
 final class TaskController extends AbstractController
 {
     #[Route('/tasks', name: 'app_task_index')]
-    public function index(TaskRepository $taskRepository): Response
-    {
-        $tasks = $taskRepository->findBy(
-            [/*'owner' => $this->getUser()*/],
-            ['createdAt' => 'DESC']
-        );
+    public function index(Request $request,
+                          TaskRepository $taskRepository
+    ): Response {
+        $status = $request->query->get('status');
+
+        if ($status === 'done') {
+            $tasks = $taskRepository->findBy(
+                [
+                    //'owner' => $this->getUser(),
+                    'isDone' => true
+                ], 
+                ['createdAt' => 'DESC']);
+        } elseif ($status === 'open') {
+            $tasks = $taskRepository->findBy(
+                [
+                    //'owner' => $this->getUser(),
+                    'isDone' => false
+                ], 
+                ['createdAt' => 'DESC']);
+        } else {
+            $tasks = $taskRepository->findBy(
+                [
+                    //'owner' => $this->getUser()
+                ], 
+                ['createdAt' => 'DESC']);
+        }
 
         return $this->render('task/index.html.twig', [
             'tasks' => $tasks,
+            'current_status' => $status
         ]);
     }
 
@@ -59,8 +80,8 @@ final class TaskController extends AbstractController
 
     #[Route('/tasks/{id}/edit', name: 'app_task_edit')]
     public function edit(Task $task,
-                        Request $request, 
-                        EntityManagerInterface $entityManager
+                         Request $request, 
+                         EntityManagerInterface $entityManager
     ): Response {
         $form = $this->createForm(TaskType::class, $task);
         $form->handleRequest($request);
@@ -79,8 +100,8 @@ final class TaskController extends AbstractController
 
     #[Route('/tasks/{id}/delete', name: 'app_task_delete', methods: ['POST'])]
     public function delete(Task $task,
-                        Request $request, 
-                        EntityManagerInterface $entityManager
+                           Request $request, 
+                           EntityManagerInterface $entityManager
     ): Response {
         if ($this->isCsrfTokenValid('delete'.$task->getId(), $request->request->get('_token'))) {
             $entityManager->remove($task);
@@ -93,10 +114,9 @@ final class TaskController extends AbstractController
     }
 
     #[Route('/tasks/{id}/toggle', name: 'app_task_toggle', methods: ['POST'])]
-    public function toggle(
-        Task $task,
-        Request $request,
-        EntityManagerInterface $entityManager
+    public function toggle(Task $task,
+                           Request $request,
+                           EntityManagerInterface $entityManager
     ): Response {
         if ($this->isCsrfTokenValid('toggle'.$task->getId(), $request->request->get('_token'))) {
             $task->setIsDone(!$task->isDone());
